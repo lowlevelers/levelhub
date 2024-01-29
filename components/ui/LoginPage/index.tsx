3ca5f115f2514525a22a91b1bf5e236c01c9883b
tin-snowflake <cqtin0903@gmail.com>
2024-01-29 23:55:38 +0700

    remove redundant vercel url

--- components/ui/LoginPage/index.tsx
+++ components/ui/LoginPage/index.tsx
@@ -8,7 +8,6 @@ import { GithubProvider } from '../AuthProviderButtons';
 const getURL = () => {
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
     'https://up.lowlevelers.com';
 
   // 'http://localhost:3000/';
